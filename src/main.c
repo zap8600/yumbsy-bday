07a6d65799ebaf2968b5a8aec23fd247e3dc6e32
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-12-17 11:52:54 -0600

    update i waited too long i waited too long!!!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#define CNFG_IMPLEMENTATION
 
+#include "rawdraw_sf.h"
+
+void HandleKey(int keycode, int bDown) { }
+void HandleButton(int x, int y, int button, int bDown) { }
+void HandleMotion(int x, int y, int mask) { }
+int HandleDestroy() { return 0; }
 int main() {
-    printf("Waiting for the next birthday...\n");
-    return 0;
+	CNFGSetup( "Yumbsy bday", 1024, 768 );
+	while(CNFGHandleInput()) {
+        CNFGClearFrame();
+		CNFGSwapBuffers();		
+	}
 }
